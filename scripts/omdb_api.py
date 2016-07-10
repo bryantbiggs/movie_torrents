07ecfeb4e9d5ea86f59f330eedbb95ac3f6f4866
Bryant Biggs <bryantbiggs@users.noreply.github.com>
2016-07-10 19:19:32 -0400

    modified omdb_api to clean each call returned, convert to dataframe, and write to csv

--- scripts/omdb_api.py
+++ scripts/omdb_api.py
@@ -3,48 +3,87 @@ import re
 import pandas as pd
 import json
 from pprint import pprint
+import time
 
 def omdb_api_query():
     '''
     :return: using csv list of movies from The Numbers, gather movie data from omdb api
     '''
-    front = 'http://www.omdbapi.com/?t='
-    end = '&r=json'
-
     movie_lst = pd.read_csv('../data/movie_dollars.csv')
-    omdb_dict = {}
-    bad_lst = []
 
-    for movie in movie_lst['title']:
+    # tupple of movie, title
+    title = movie_lst['title']
+    year = [year[:4] for year in movie_lst['release_date']]
+    movie_tup = [(title, year) for title, year in zip(title, year)]
 
-        try:
-            # try api call with title as is (spaces, etc.)
-            url = front + movie + end
-            html = requests.get(url).text
-            omdb_dict[movie] = html
+    movie_dict = {}
+    count = 0
 
-            # with each call, write to file
-            with open('../data/movie.json', 'w') as f:
-                json.dump(omdb_dict, f)
-        except:
+    for title, year in movie_tup:
+        # meter number of requests to omdb api
+        time.sleep(5)
+
+        url = 'http://www.omdbapi.com/?t={0}&y={1}&plot=short&r=json'.format(title, year)
+        html = requests.get(url).text
+
+        count += 1
+        print(str(len(movie_tup)-count), html)
+
+        if 'Error' in html:
+            with open('../data/not_found.txt', 'a') as f:
+                f.write(title + '\n')
+        else:
+            # remove curly braces and split to list
+            movie_meta = re.sub(r'{', '', html)
+            movie_meta = re.sub(r'}', '', movie_meta)
+            movie_meta = movie_meta.split('","')
+
+            # remove all backslashes
+            for i in range(len(movie_meta)):
+                movie_meta[i] = re.sub('\"', '', movie_meta[i])
+
+            labels, info = [], []
+
+            # extract labels and data
+            for meta in movie_meta:
+                temp = meta.split(':')
+                labels.append(temp[0])
+                info.append(temp[1])
+
+            # add to dictionary
+            movie_dict[title] = info
+
+            if count == 1:
+                # first time through, add labels
+                df = pd.DataFrame.from_dict(movie_dict, orient='index')
+                df.columns = labels
+            else:
+                df = pd.DataFrame.from_dict(movie_dict, orient='index')
+
+            # write to csv
+            df.to_csv('../data/omdb_data.csv', mode='w', header=labels, index=False)
+
+        #except:
             # remove all special characters and replace whitespace with '+'
-            movie_api = re.sub(r'[^a-zA-Z0-9 ]*', '', movie)
-            movie_api = re.sub(' ', '+', movie_api)
+            #movie_api = re.sub(r'[^a-zA-Z0-9 ]*', '', movie)
+            #movie_api = re.sub(' ', '+', movie_api)
 
-            try:
+            #try:
                 # try api call with modified title
-                url = front + movie_api + end
-                html = requests.get(url).text
-                omdb_dict[movie] = html
+            #    url = front + movie_api + end
+            #    html = requests.get(url).text
+            #    omdb_dict[movie] = html
 
                 # with each call, write to file
-                with open('../data/movie.json', 'w') as f:
-                    json.dump(omdb_dict, f)
-            except:
-                # show me any movies that didn't make the api call
-                bad_lst.append(movie)
-                with open('../data/bad_omdb.txt', 'w') as f:
-                    f.write(bad_lst)
+                #with open('../data/movie.json', 'w') as f:
+                #    json.dump(omdb_dict, f)
+            #except:
+
+            # show me any movies that didn't make the api call
+            #bad_lst.append(movie)
+
+            #with open('../data/bad_omdb.txt', 'a') as f:
+            #    f.write(str(movie))
 
 def strip_html():
     with open('../data/movie.json') as json_data:
@@ -105,8 +144,7 @@ def omdb_json_clean():
         #with open('../data/clean.txt', 'w') as wr_file:
         #    wr_file.write(line)
 
-#strip_html()
-omdb_json_clean()
+omdb_api_query()
 
 if __name__ is '__main__':
     omdb_api_query()
