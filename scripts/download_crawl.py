6efcec96a59a1b961766abeb87b4c5dc40a1a1d1
Bryant Biggs <bryantbiggs@users.noreply.github.com>
2016-07-11 14:32:07 -0400

    kickass media count crawler complete

--- scripts/download_crawl.py
+++ scripts/download_crawl.py
@@ -4,35 +4,46 @@ import re
 import pandas as pd
 import os
 
-def kat_crawl():
-
-    # remove not found file
-    os.remove('../data/kat_notfound.txt')
-
+def download_crawl():
     # get imdb id
     omdb_data = pd.read_csv('../data/omdb_data.csv', encoding='latin-1')
 
     for imdb_num in omdb_data['imdbID']:
+        kat_crawl(imdb_num)
+        omdb_data.loc[omdb_data.imdbID == imdb_num, 'Kat_Count'] = kat_crawl(imdb_num)
 
-        try:
-            # try to get media count
-            url = 'https://kat.cr/usearch/category:movies%20imdb:{0}/'.format(imdb_num[2:])
-            soup = BeautifulSoup(requests.get(url).text, 'lxml')
+        omdb_data.to_csv('../data/text_data.csv', mode='w', index=False)
 
-            result = soup.div.h2.span
+def kat_crawl(imdb_num):
 
-            result = re.sub(r'(<span>  results )([0-9*]\D[0-9*]*)( from )', '', str(result))
-            result = re.sub(r'(</span>)', '', result)
+    # remove not found file for each run
+    kat_path = '../data/kat_notfound.txt'
+    if os.path.isfile(kat_path): os.remove(kat_path)
 
-        except:
-            # otherwise, return 0 and write to not found file
-            with open('../data/kat_notfound.txt', 'a') as f:
+    try:
+        # try to get media count
+        url = 'https://kat.cr/usearch/category:movies%20imdb:{0}/'.format(imdb_num[2:])
+        soup = BeautifulSoup(requests.get(url).text, 'lxml')
+
+        result = soup.div.h2.span
+        result = re.sub(r'(<span>  results )([0-9*]\D[0-9*]*)( from )', '', str(result))
+        result = re.sub(r'(</span>)', '', result)
+
+    except:
+
+        # otherwise, return 0 and write to not found file
+        if os.path.isfile(kat_path):
+            with open(kat_path, 'a') as f:
+                f.write(imdb_num + '\n')
+            result = 0
+        else:
+            with open(kat_path, 'w') as f:
                 f.write(imdb_num + '\n')
             result = 0
 
     return result
 
-kat_crawl()
+download_crawl()
 
 if __name__ is '__main__':
-    kat_crawl()
+    download_crawl()
