8402018dda8dc2838116311008b24530c303e3de
Bryant Biggs <bryantbiggs@users.noreply.github.com>
2016-07-12 17:11:15 -0400

    added in directors and actors

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,28 +1,43 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
 
-torrent_df = pd.read_csv('../data/torrent_data.csv', encoding='latin-1')
-torrent_df = torrent_df[['Title', 'Released', 'Genre', 'Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']]
-# convert to datetime
-torrent_df['Released'] = pd.to_datetime(torrent_df['Released'])
+def clean():
+       torrent_df = pd.read_csv('../data/torrent_data.csv', encoding='latin-1')
+       torrent_df = torrent_df[['Title', 'Released', 'Genre', 'Director', 'Actors', 'Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']]
+       # convert to datetime
+       torrent_df['Released'] = pd.to_datetime(torrent_df['Released'])
+       print('Data points of torrent_df = ' + str(len(torrent_df)))
 
-financial_df = pd.read_csv('../data/movie_dollars.csv', encoding='latin-1')
-financial_df = financial_df.rename(columns={'title':'Title', 'release_date':'Released', 'prod_budget':'Prod_Budget', 'dom_gross':'Dom_Gross', 'world_gross':'World_Gross'})
-# convert to datetime
-financial_df['Released'] = pd.to_datetime(financial_df['Released'])
+       financial_df = pd.read_csv('../data/movie_dollars.csv', encoding='latin-1')
+       financial_df = financial_df.rename(columns={'title':'Title', 'release_date':'Released', 'prod_budget':'Prod_Budget'})
+       financial_df = financial_df[['Title', 'Released', 'Prod_Budget']]
+       # convert to datetime
+       financial_df['Released'] = pd.to_datetime(financial_df['Released'])
+       print('Data points of financial_df = ' + str(len(financial_df)))
 
-# inner join to get uniques
-data_df = pd.merge(financial_df, torrent_df, how='outer', on=['Title', 'Released'])
-print(len(data_df))
+       # merge data frames
+       data_df = pd.merge(financial_df, torrent_df, how='inner', on=['Title', 'Released'])
+       print('Data points AFTER merge, BEFORE clean = ' + str(len(data_df)))
 
-# drop empty cells
-data_df = data_df.replace('', np.nan, regex=True)
-data_df =  data_df.dropna(how='any', subset=['Prod_Budget', 'Genre', 'Torrentz_Count'])
 
-# drop failed or non-numeric torrent count cells
-data_df[['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']] = data_df[['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']].convert_objects(convert_numeric=True)
-data_df =  data_df.dropna(how='any', subset=['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count'])
-print(len(data_df))
-print(data_df.head(20))
-# drop out that squeaky clean
-data_df.to_csv('../data/data.csv', sep=',', index=False)
+       # drop empty cells
+       data_df = data_df.replace('', np.nan, regex=True)
+       data_df =  data_df.dropna(how='any', subset=['Prod_Budget', 'Genre', 'Torrentz_Count'])
+
+       # drop failed or non-numeric torrent count cells
+       data_df[['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']] = data_df[['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']].convert_objects(convert_numeric=True)
+       data_df =  data_df.dropna(how='any', subset=['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count'])
+       print('Data points AFTER merge AND clean = ' + str(len(data_df)))
+
+       # sum torrent counts
+       data_df['Total_Torrents'] = data_df[['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']].sum(axis=1)
+       # drop out that squeaky clean
+       data_df.to_csv('../data/data.csv', sep=',', index=False)
+
+clean()
+
+if __name__ is '__main__':
+       clean()
