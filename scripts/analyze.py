72c42a6709fe4e9bff4a92a07bdc5f4502199cff
Bryant Biggs <bryantbiggs@users.noreply.github.com>
2016-07-14 16:57:56 -0400

    updated script to add year and month columns, removed torrent columns other than torrent_total, added train and test split to two separate csv files

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,22 +1,27 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.style.use('ggplot')
+from sklearn.cross_validation import train_test_split
 
 def clean():
+       # read in torrent_data
        torrent_df = pd.read_csv('../data/torrent_data.csv', encoding='latin-1')
        torrent_df = torrent_df[['Title', 'Rated', 'Released', 'Runtime', 'Genre', 'Director', 'Actors', 'Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']]
-       # convert to datetime
+
+       # convert released date to datetime
        torrent_df['Released'] = pd.to_datetime(torrent_df['Released'])
-       # convert to number
+       torrent_df['Year'] = pd.DatetimeIndex(torrent_df['Released']).year
+       torrent_df['Month'] = pd.DatetimeIndex(torrent_df['Released']).month
+
+       # convert runtime to number
        torrent_df['Runtime'] = torrent_df['Runtime'].str.rstrip(' min')
        print('Data points of torrent_df = ' + str(len(torrent_df)))
 
+       # read in financial data
        financial_df = pd.read_csv('../data/movie_dollars.csv', encoding='latin-1')
        financial_df = financial_df.rename(columns={'title':'Title', 'release_date':'Released', 'prod_budget':'Prod_Budget'})
        financial_df = financial_df[['Title', 'Released', 'Prod_Budget']]
-       # convert to datetime
+
+       # convert released date to datetime
        financial_df['Released'] = pd.to_datetime(financial_df['Released'])
        print('Data points of financial_df = ' + str(len(financial_df)))
 
@@ -29,14 +34,23 @@ def clean():
        data_df =  data_df.dropna(how='any', subset=['Prod_Budget', 'Genre', 'Torrentz_Count'])
 
        # drop failed or non-numeric torrent count cells
-       data_df[['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']] = data_df[['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']].convert_objects(convert_numeric=True)
+       data_df[['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']] = data_df[['Pirate_Count', 'Torrentz_Count',
+                                                                                  'Zoogle_Ver_Count']].convert_objects(convert_numeric=True)
        data_df =  data_df.dropna(how='any', subset=['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count'])
        print('Data points AFTER merge AND clean = ' + str(len(data_df)))
 
        # sum torrent counts
        data_df['Total_Torrents'] = data_df[['Pirate_Count', 'Torrentz_Count', 'Zoogle_Ver_Count']].sum(axis=1)
+
+       # trim to pertinent columns
+       data_df = data_df[['Title', 'Released', 'Year', 'Month', 'Rated', 'Runtime', 'Genre', 'Director', 'Actors', 'Total_Torrents']]
+
+       train_df, test_df = train_test_split(data_df, train_size=0.80, random_state=1)
+
        # drop out that squeaky clean
        data_df.to_csv('../data/data.csv', sep=',', index=False)
+       train_df.to_csv('../data/train_data.csv', sep=',', index=False)
+       test_df.to_csv('../data/test_data.csv', sep=',', index=False)
 
 clean()
 
