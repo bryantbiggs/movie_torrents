46c07225b62ea9a26acb0910835ad2b280741fa8
Bryant Biggs <bryantbiggs@users.noreply.github.com>
2016-07-25 08:29:19 -0400

    file cleanup

--- scripts/omdb_api.py
+++ scripts/omdb_api.py
@@ -2,35 +2,46 @@ import requests
 import re
 import pandas as pd
 import time
-import string
+import os
 
 def omdb_api_query():
-    '''
-    :return: using csv list of movies from The Numbers, gather movie data from omdb api
-    '''
-    movie_lst = pd.read_csv('../data/movie_dollars.csv', encoding='latin-1')
+    # files
+    write_file = '../data/omdb.csv'
+    read_file = '../data/budget.csv'
+    err_file = '../data/omdb_error.csv'
+
+    # remove any previously generated files
+    if os.path.isfile(err_file): os.remove(err_file)
+    if os.path.isfile(write_file): os.remove(write_file)
+
+    # use encoding='latin-1' to steamroll encoding issues
+    movie_lst = pd.read_csv(read_file, encoding='latin-1')
 
     # tupple of movie, title
-    title = movie_lst['title']
-    year = [year[:4] for year in movie_lst['release_date']]
+    title = movie_lst['Title']
+    year = [year[:4] for year in movie_lst['Released']]
     movie_tup = [(title, year) for title, year in zip(title, year)]
 
     count = 0
 
     for title, year in movie_tup:
         # meter number of requests to omdb api
-        time.sleep(5)
+        time.sleep(2)
 
         try:
+            # omdb api address
             url = 'http://www.omdbapi.com/?t={0}&y={1}&plot=short&r=json'.format(title, year)
             html = requests.get(url).text
 
+            # progress output to console
             count += 1
-            print(str(len(movie_tup)-count), html)
+            #print(str(len(movie_tup)-count), html)
 
+            # if api call returned an error, write movie title and year to file
             if 'Error' in html:
-                with open('../data/not_found.txt', 'a') as f:
-                    f.write(title + '\n')
+                err(title, year)
+
+            # otherwise, api call returned movie data - process it
             else:
                 # remove curly braces and split to list
                 movie_meta = re.sub(r'{', '', html)
@@ -50,42 +61,26 @@ def omdb_api_query():
                     info.append(temp[1])
 
                 # add to dictionary
-                movie_dict = {}
-                movie_dict[title] = info
+                movie_dict = {title: info}
 
                 if count == 1:
                     # first time through, add labels
                     df = pd.DataFrame.from_dict(movie_dict, orient='index')
                     df.columns = labels
-
-                    # write to csv
-                    df.to_csv('../data/omdb_data.csv', mode='w', header=labels, index=False)
+                    df.to_csv(write_file, mode='w', header=labels, index=False)
                 else:
                     df = pd.DataFrame.from_dict(movie_dict, orient='index')
-                    # write to csv
-                    df.to_csv('../data/omdb_data.csv', mode='a', header=False, index=False)
+                    df.to_csv(write_file, mode='a', header=False, index=False)
 
+        # if api call could not find title, year then write movie title and year to file
         except:
-            with open('../data/not_found.txt', 'a') as f:
-                f.write(title + '\n')
+            err(title, year)
 
-def modified_query():
 
-    # load financial omdb data into dataframe
-    movie_lst = pd.read_csv('../data/movie_dollars.csv', encoding='latin-1')
-    movie_lst['year'] = pd.to_datetime(movie_lst['release_date'])
-
-    # remove punctuation
-    movie_lst['strip_title'] = movie_lst['title'].apply(lambda x:''.join([i for i in x if i not in string.punctuation]))
-
-    with open('../data/not_found.txt', encoding='latin-1') as f:
-        #titles = [line.strip() for line.apply(lambda x:''.join([i for i in x if i not in string.punctuation])) in f]
-        titles = [''.join(x) for x in title if x not in string.punctuation for title in f]
-
-    #print(movie_lst.loc[movie_lst['title'] == titles])
-    print(titles)
-
-modified_query()
+def err(title, year):
+    # log movies that failed to return data from omdb api
+    with open('../data/omdb_error.csv', 'a') as f:
+        f.write('{0}, {1}\n'.format(title, year))
 
 if __name__ is '__main__':
     omdb_api_query()
--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,88 +1,54 @@
-import requests
-import re
 import pandas as pd
 from bs4 import BeautifulSoup
 import codecs
 
 def movie_dollars():
-    '''
-    input: webpage manually saved as html
-    output: a clean csv of the movie financial data
-    '''
-
-    url = "../data/The Numbers - Movie Budgets.html"
+    # files
+    # webpage was manually saved to directory
+    read_file = "../data/The Numbers - Movie Budgets.html"
+    write_file = '../data/budget.csv'
 
     # use encoding='latin-1' to steamroll encoding issues
-    with codecs.open(url, "r", encoding="latin-1") as fdata:
+    with codecs.open(read_file, "r", encoding="latin-1") as fdata:
+
         soup = BeautifulSoup(fdata.read(), 'lxml')
 
-        # get all data in table in ordered list
-        movie_table = [link.text for link in soup.select('td ')]
+        # put all table data into an ordered list
+        movie_tbl = [link.text for link in soup.select('td ')]
 
         # chunk data to groups of 6 (6 colums in original table)
-        movie_lst = [movie_table[i:i + 6] for i in range(0, len(movie_table), 6)]
+        movie_lst = [movie_tbl[i:i + 6] for i in range(0, len(movie_tbl), 6)]
 
-        # move data into dict to send to pandas
-        movie_dict = {}
-        for movie in movie_lst:
-            movie_dict[movie[0]] = movie[1:]
+        # move data into dictionary to send to pandas dataframe
+        movie_dict = {movie[0]: movie[1:] for movie in movie_lst}
 
-        # birth a panda
+        # give birth to a panda
         movie_df = pd.DataFrame.from_dict(movie_dict, orient='index')
 
         # add column names and re-order
-        movie_df.columns = ['release_date', 'title', 'prod_budget', 'dom_gross', 'world_gross']
-        movie_df = movie_df[['title', 'release_date', 'prod_budget', 'dom_gross', 'world_gross']]
+        movie_df.columns = ['Released', 'Title', 'Prod_Budget', 'Dom_Gross', 'World_Gross']
+        movie_df = movie_df[['Title', 'Released', 'Prod_Budget', 'Dom_Gross', 'World_Gross']]
 
         # convert currencies to floating point values
-        movie_df[['prod_budget']] = movie_df[['prod_budget']].replace('[\$,]', '', regex=True).astype(float)
-        movie_df[['dom_gross']] = movie_df[['dom_gross']].replace('[\$,]', '', regex=True).astype(float)
-        movie_df[['world_gross']] = movie_df[['world_gross']].replace('[\$,]', '', regex=True).astype(float)
+        movie_df[['Prod_Budget']] = movie_df[['Prod_Budget']].replace('[\$,]', '', regex=True).astype(float)
+        movie_df[['Dom_Gross']] = movie_df[['Dom_Gross']].replace('[\$,]', '', regex=True).astype(float)
+        movie_df[['World_Gross']] = movie_df[['World_Gross']].replace('[\$,]', '', regex=True).astype(float)
 
-        assert movie_df['prod_budget'].dtype == 'float64'
-        assert movie_df['dom_gross'].dtype == 'float64'
-        assert movie_df['world_gross'].dtype == 'float64'
+        assert movie_df['Prod_Budget'].dtype == 'float64'
+        assert movie_df['Dom_Gross'].dtype == 'float64'
+        assert movie_df['World_Gross'].dtype == 'float64'
 
         # convert release date to datetime
-        movie_df['release_date'] = pd.to_datetime(movie_df['release_date'], format='%m/%d/%Y')
+        movie_df['Released'] = pd.to_datetime(movie_df['Released'], format='%m/%d/%Y')
 
-        assert movie_df['release_date'].dtype == 'datetime64[ns]'
+        assert movie_df['Released'].dtype == 'datetime64[ns]'
 
-        # order largest budget to least
-        movie_df = movie_df.sort_values(by='prod_budget', ascending=False).reset_index()
+        # re-order dataframe on production budget column largest to smallest, delete old index
+        movie_df = movie_df.sort_values(by='Prod_Budget', ascending=False).reset_index()
         del movie_df['index']
 
-        # drop out that squeaky clean
-        movie_df.to_csv('../data/movie_dollars.csv', sep=',', index=False)
-
-def mojo_genres():
-    '''
-    :return: return tuple of genre info as (genre, ending suffix for genre link)
-    '''
-    url = 'http://www.boxofficemojo.com/genres/'
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, 'lxml')
-
-    # function to filter href tags by regex expression
-    def genre_title(href):
-        return href and re.compile("\.\/chart\/\?id\=").search(href)
-
-    # pull href tags based on filter
-    genre_hrefs = soup.find_all(href=genre_title)
-
-    # list of genres
-    genre_lst = [re.sub('<[^>]+>', '', str(i)) for i in genre_hrefs]
-
-    # list of links to navigate to genres
-    genre_link_lst = [re.sub(r'(<[^>]+="./)', '', str(i)) for i in genre_hrefs]
-    genre_link_lst = [re.sub(r'(>[^>]+>)', '', str(i)) for i in genre_link_lst]
-
-    # pack up genres with links to there page in a tuple
-    genre_tup = [(genre, link) for genre, link in zip(genre_lst, genre_link_lst)]
-    return genre_tup
-
-movie_dollars()
+        # drop out clean csv
+        movie_df.to_csv(write_file, sep=',', index=False)
 
 if __name__ is '__main__':
     movie_dollars()
-    mojo_genres()
